d18aa2b82d8d002c35e49c24396afc2b4f7c75ed
s6452410018 <s6452410018@live.sau.ac.th>
2025-01-16 15:00:56 +0700

    V.2 Welcome UI completed

---
+++ lib/views/signup_ui.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SignUpUI extends StatefulWidget {
+  const SignUpUI({super.key});
+
+  @override
+  State<SignUpUI> createState() => _SignUpUIState();
+}
+
+class _SignUpUIState extends State<SignUpUI> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
