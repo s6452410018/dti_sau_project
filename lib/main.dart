856361fd7a105c1726d310e3da718f7c8d92bf1d
s6452410018 <s6452410018@live.sau.ac.th>
2025-01-23 13:12:09 +0700

    V.3 Finished all UI

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:dti_sau_project/views/login_ui.dart';
+import 'package:dti_sau_project/views/signup_ui.dart';
 import 'package:dti_sau_project/views/welcome_ui.dart';
 import 'package:flutter/material.dart';
 
@@ -22,7 +23,7 @@ class _DtiSauState extends State<DtiSau> {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: LoginUI(),
+      home: SignUpUI(),
     );
   }
 }
